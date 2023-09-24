a306269345a78a7bd380460c0eed0784e4e8a96b
Yash Pandey <pandeyyash041@gmail.com>
2023-09-24 11:03:43 +0530

    Modified sum.java

--- sum.java
+++ sum.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class sum {
     public static void main(String args[]) {
         int a = 4, b = 5;
-        System.out.println("SUM OF " + a + " AND " + b + " IS ::: " + (a + b));
+        int sum = a + b;
+        System.out.println("SUM OF " + a + " AND " + b + " IS ::: " + (sum));
     }
 }
