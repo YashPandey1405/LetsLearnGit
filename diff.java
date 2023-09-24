6013fc1d209d0d247c60eb5461b4431e90da583c
Yash Pandey <pandeyyash041@gmail.com>
2023-09-24 11:00:51 +0530

    adding diff.java

---
+++ diff.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class diff {
+    public static void main(String args[]) {
+        int a = 4, b = 5;
+        int diff = a - b;
+        System.out.println("DIFF. OF " + a + " AND " + b + " IS ::: " + (diff));
+    }
+}
