2397acb0e81302c4c6a49f749f915244b7229fbf
Yash Pandey <pandeyyash041@gmail.com>
2023-09-24 10:50:59 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class sum {
+    public static void main(String args[]) {
+        int a = 4, b = 5;
+        System.out.println("SUM OF " + a + " AND " + b + " IS ::: " + (a + b));
+    }
+}
