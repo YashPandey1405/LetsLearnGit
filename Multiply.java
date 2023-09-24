be351de1ab8c71bf84028be9e7f68df32383a28c
Yash Pandey <pandeyyash041@gmail.com>
2023-09-24 11:23:54 +0530

    Added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,6 @@
+public class Multiply {
+    public static void main(String args[]) {
+        int a = 5, b = 4;
+        System.out.println("THE PPRODUCT OF " + a + " AND " + b + " IS ::: " + (a * b));
+    }
+}
